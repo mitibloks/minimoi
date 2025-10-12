e1bca167d76a5f857c10906060f9ee5e85741693
mitibloks <hagiaahmad06@gmail.com>
2025-10-12 20:23:39 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var map = L.map('map').setView([-6.9175, 107.6191], 13);
+  var map = L.map('map').setView([-6.920753, 107.667703], 13);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
   }).addTo(map);
 
-  L.marker([-6.920757, 107.667676]).addTo(map)
+  L.marker([-6.920753, 107.667703]).addTo(map)
     .bindPopup("<b>minimoi Arcamanik</b><br>Jl. Puri Dago XI No. 4");
 });
